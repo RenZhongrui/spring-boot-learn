7f9efe0939870d5173d51ca1fa5c5c84049c7ca5
renzhongrui <zrzoffice@163.com>
2019-09-14 16:19:14 +0800

    自定义日志级别颜色

--- 001-spring-boot-log/src/main/java/com/learn/log/Application.java
+++ 001-spring-boot-log/src/main/java/com/learn/log/Application.java
@@ -1,5 +1,6 @@
 package com.learn.log;
 
+import com.learn.log.schedule.ScheduleTest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ 001-spring-boot-log/src/main/java/com/learn/log/schedule/ScheduleTest.java
@@ -0,0 +1,11 @@
+package com.learn.log.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ScheduleTest {
+
+    public void test() {
+        log.debug("ScheduleTest");
+    }
+}
